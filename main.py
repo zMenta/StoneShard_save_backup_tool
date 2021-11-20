be5c6b4f68006419bb6dd76940e53b0b577a2b68
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-11-20 14:47:52 -0300

    Basic exitsave copy

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import shutil
 local_directory = os.getcwd()
 # print(local_directory)
 
-# os.mkdir(local_directory + "/folder")
 
-os.unlink(local_directory + "/folder")
+# os.rmdir(local_directory + "/folder") #remove directory / remove folder
+
+
+# shutil.copy("C:/Users/brent/AppData/Local/StoneShard/characters_v1/character_1/exitsave_1/save.map", local_directory)
+save_backup_directory = local_directory + "/exitsave_1"
+os.mkdir(save_backup_directory)
+
+exitsave_path = "C:/Users/brent/AppData/Local/StoneShard/characters_v1/character_1/exitsave_1"
+
+exitsave_files = os.listdir(exitsave_path)
+print(exitsave_files)
+
+for file in exitsave_files:
+    shutil.copy("C:/Users/brent/AppData/Local/StoneShard/characters_v1/character_1/exitsave_1/" + file, save_backup_directory)
