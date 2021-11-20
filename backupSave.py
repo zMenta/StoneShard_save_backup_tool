b3789bdda4b100734402e24e2c11453b5cf50a54
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-11-20 19:06:45 -0300

    backUp fix and improvement

--- backupSave.py
+++ backupSave.py
@@ -2,33 +2,39 @@ import os
 import shutil
 import json
 
-
-with open('config.json') as file:
-    data = json.load(file)
-
-save_backup_directory = data['save_backup_directory'] + "/exitsave_1"
-exitsave_path = data["stoneShard_save_directory"]
-backup_exist = os.path.isdir(save_backup_directory)
-exitsave_path_exists = os.path.isdir(exitsave_path)
-
 def backupSave():
-    print("--BACKUP SAVE START--")
-    # Creates or remove the backup files #
-    if(backup_exist):
-        save_backup_directory_files = os.listdir(save_backup_directory)
-        for file in save_backup_directory_files:
-            path = os.path.join(save_backup_directory, file)
-            os.remove(path)
-    else:
-        os.mkdir(save_backup_directory)
+    with open('config.json') as file:
+        data = json.load(file)
 
+    backup_directory = data['save_backup_directory'] + "/exitsave_1"
+    exitsave_path = data["stoneShard_save_directory"]
+    backup_exist = os.path.isdir(backup_directory)
+    exitsave_path_exists = os.path.isdir(exitsave_path)
 
-    # Copy save files into the backup #
-    if(exitsave_path_exists):
-        exitsave_files = os.listdir(exitsave_path)
-        for file in exitsave_files:
-            shutil.copy(exitsave_path + "/" + file, save_backup_directory)
-        print("\t--DONE--")
+    print("--START: BACKUP SAVE--")
+    # Create directory if backup don't exists #
+    if not backup_exist:
+        os.mkdir(backup_directory)
+        print("--backup path don't exist--")
+        print("--backup path created--")
+        print("--please try again--")
+
     else:
-        print("--exitsave_1 path don't exist--")
+        # Remove the backup files #
+        if(exitsave_path_exists):
+            backup_directory_files = os.listdir(backup_directory)
+            for file in backup_directory_files:
+                path = os.path.join(backup_directory, file)
+                os.remove(path)
+
+            # Copy save files into the backup #
+            exitsave_files = os.listdir(exitsave_path)
+            for file in exitsave_files:
+                shutil.copy(exitsave_path + "/" + file, backup_directory)
+            print("\t--DONE--")
+
+        else:
+            print("--stoneshard exitsave_1 path don't exist--")
+
+backupSave()
 
--- insertSave.py
+++ insertSave.py
@@ -2,31 +2,40 @@ import os
 import shutil
 import json
 
-
-with open('config.json') as file:
-    data = json.load(file)
-
-backup_directory = data['save_backup_directory'] + "/exitsave_1"
-exitsave_path = data["stoneShard_save_directory"]
-backup_exist = os.path.isdir(backup_directory)
-exitsave_exists = os.path.isdir(exitsave_path)
-
 def insertSave():
-    print("--APPLY SAVE START--")
-    # Creates or remove the stoneshard save files #
+
+    with open('config.json') as file:
+        data = json.load(file)
+
+    backup_directory = data['save_backup_directory'] + "/exitsave_1"
+    exitsave_path = data["stoneShard_save_directory"]
+    backup_exist = os.path.isdir(backup_directory)
+    exitsave_exists = os.path.isdir(exitsave_path)
+    
+    print("--START: APPLY SAVE--")
+    # Remove the stoneshard save file if the file exists#
     if(exitsave_exists):
         stoneshard_save_files= os.listdir(exitsave_path)
         for file in stoneshard_save_files:
             path = os.path.join(exitsave_path, file)
             os.remove(path)
-    else:
-        os.mkdir(exitsave_path)
+        os.rmdir(exitsave_path)
 
-    # Insert backup files #
-    if(backup_exist):
+    def insertFiles():
+        os.mkdir(exitsave_path)
         backup_files = os.listdir(backup_directory)
         for file in backup_files:
-            shutil.copy(backup_directory + "/" + file, exitsave_path)
+            shutil.copy(backup_directory + "/" + file, exitsave_path + "/exitsave_1")
         print("\t--DONE--")
+
+    # Insert backup files #
+    backup_files = os.listdir(backup_directory)
+    print(backup_exist)
+    if(backup_exist and len(backup_files) != 0):
+        if exitsave_exists:
+            os.rmdir(exitsave_path)
+            insertFiles()
+        else:
+            insertFiles()
     else:
         print("--backup don't exist--")
