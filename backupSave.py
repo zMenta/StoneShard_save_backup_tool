81be71c9663985ca7238ca887c3dc1bb569f756d
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-11-20 15:31:37 -0300

    backup path exist verification

---
+++ backupSave.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import json
+
+# Use .txt later for easier use.
+#Load CONFIG #
+config_file = open('config.json')
+config = json.load(config_file)
+
+this_directory = os.getcwd()
+save_backup_directory = os.path.join(this_directory, "exitsave_1") #this_directory + "/exitsave_1"
+backup_exist = os.path.isdir(save_backup_directory)
+
+print (save_backup_directory)
+print(os.path.join(save_backup_directory, "data.sav"))
+
+# Creates or Overwrite the backup file #
+if(backup_exist):
+    save_backup_directory_files = os.listdir(save_backup_directory)
+    print(save_backup_directory_files)
+    for file in save_backup_directory_files:
+        path = os.path.join(save_backup_directory, file)
+        print(path)
+        os.remove(path)
+else:
+    os.mkdir(save_backup_directory)
+
+exitsave_path = "C:/Users/brent/AppData/Local/StoneShard/characters_v1/character_1/exitsave_1"
+
+exitsave_files = os.listdir(exitsave_path)
+# print(exitsave_files)
+
+for file in exitsave_files:
+    shutil.copy("C:/Users/brent/AppData/Local/StoneShard/characters_v1/character_1/exitsave_1/" + file, save_backup_directory)
