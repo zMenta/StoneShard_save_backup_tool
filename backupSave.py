0dffc0861b18b60d5580372cc6e9342354800f91
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-11-20 17:34:43 -0300

    wip

--- backupSave.py
+++ backupSave.py
@@ -2,7 +2,6 @@ import os
 import shutil
 import json
 
-print("\033[0;32mSTART\033[0;0;0m")
 
 with open('config.json') as file:
     data = json.load(file)
@@ -12,22 +11,24 @@ exitsave_path = data["stoneShard_save_directory"]
 backup_exist = os.path.isdir(save_backup_directory)
 exitsave_path_exists = os.path.isdir(exitsave_path)
 
-# Creates or remove the backup files #
-if(backup_exist):
-    save_backup_directory_files = os.listdir(save_backup_directory)
-    for file in save_backup_directory_files:
-        path = os.path.join(save_backup_directory, file)
-        os.remove(path)
-else:
-    os.mkdir(save_backup_directory)
+def backupSave():
+    print("--START--")
+    # Creates or remove the backup files #
+    if(backup_exist):
+        save_backup_directory_files = os.listdir(save_backup_directory)
+        for file in save_backup_directory_files:
+            path = os.path.join(save_backup_directory, file)
+            os.remove(path)
+    else:
+        os.mkdir(save_backup_directory)
 
 
-# Copy save files into the backup #
-if(exitsave_path_exists):
-    exitsave_files = os.listdir(exitsave_path)
-    for file in exitsave_files:
-        shutil.copy(exitsave_path + "/" + file, save_backup_directory)
-    print("\033[0;32mDONE\033[0;0;0m")
-else:
-    print("\033[0;31mexitsave_1 path don't exist\033[0;0;0m")
+    # Copy save files into the backup #
+    if(exitsave_path_exists):
+        exitsave_files = os.listdir(exitsave_path)
+        for file in exitsave_files:
+            shutil.copy(exitsave_path + "/" + file, save_backup_directory)
+        print("--DONE--")
+    else:
+        print("--exitsave_1 path don't exist--")
 
--- run.py
+++ run.py
@@ -1,7 +1,20 @@
-# import backupSave
+from backupSave import backupSave
 # import insertSave
+import json
 import keyboard
 
+with open('config.json') as file:
+    data = json.load(file)
+
+backup_key = data["backup_key"]
+insert_key = data["insert_save_key"]
+exit_key = data["exit_key"]
+
 while True:
-    if keyboard.is_pressed('q'):
-        print(keyboard.read_key())
+    print(keyboard.read_key())
+    if keyboard.is_pressed(backup_key):
+        backupSave()
+    if keyboard.is_pressed(insert_key):
+        print("batata")
+    if keyboard.is_pressed(exit_key):
+        break
