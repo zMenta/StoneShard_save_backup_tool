6480574fcaf6d57fe8f4beebaff1ed4ecfb0b679
Rob Adams Jr <crazypants010@gmail.com>
2022-04-20 21:20:58 -0500

    Updated
    User friendly and updated the code to detect and create the directories in the local appdata.

--- backup_save.py
+++ backup_save.py
@@ -4,48 +4,60 @@ import shutil
 
 from settings import load_config
 
+
 def backup_save(config):
-    backup_directory = op.join(config["backup_directory"], "exitsave_1")
+    backup_directory = op.join(config["backup_directory"])
     backup_directory_exists = op.isdir(backup_directory)
 
-    stone_shard_directory = config["stone_shard_directory"]
-    stone_shard_directory_exists = op.isdir(stone_shard_directory)
+    stoneshard_directory = config["stoneshard_directory"]
+    stoneshard_directory_exists = op.isdir(stoneshard_directory)
 
-    print("--[backup_save]: START --")
+    print("----------- [Backup_Save]: W O R K I N G ----------")
+    print("---------------------------------------------------\n")
     if not backup_directory_exists:
         os.mkdir(backup_directory)
         backup_directory_exists = op.isdir(backup_directory)
-        print(f"[backup_save]: create backup folder at {backup_directory}")
+        print(f"-- [Backup_Save]: Creating A Backup Folder In \n\n{backup_directory}")
+        print("---------------------------------------------------")
 
-    if not stone_shard_directory_exists:
-        print(f"[backup_save]: StoneShard directory don't exist {stone_shard_directory}")
+    if not stoneshard_directory_exists:
+        print(
+            f"-- [Backup_Save]: The Stoneshard Directory\n\n{stoneshard_directory}\n\nDoesn't Exist --"
+        )
+        print("---------------------------------------------------")
         return
 
-    if len(os.listdir(stone_shard_directory)) != 3:
-        print(f"[backup_save]: StoneShard folder at {stone_shard_directory} must have 3 files inside")
+    if len(os.listdir(stoneshard_directory)) != 3:
+        print(
+            f"-- [Backup_Save]: The Folder\n\n{stoneshard_directory}\n\nMust Have 3 Files Inside It --"
+        )
+        print("---------------------------------------------------")
         return
 
     if backup_directory_exists:
-        stone_shard_number_of_files = len(os.listdir(stone_shard_directory))
-        # Remove the backup files if Stone_shard_directory has 3 files #
-        if(stone_shard_directory_exists and stone_shard_number_of_files == 3):
+        stone_shard_number_of_files = len(os.listdir(stoneshard_directory))
+        if stoneshard_directory_exists and stone_shard_number_of_files == 3:
             backup_directory_files = os.listdir(backup_directory)
             for file in backup_directory_files:
                 path = op.join(backup_directory, file)
                 os.remove(path)
-            print(f"[backup_save]: Removed backup files at {backup_directory}")
-
-            # Copy save files into the backup #
-            stone_shard_files = os.listdir(stone_shard_directory)
+            print(f"-- [Backup_Save]: Removed The Backup Files In:\n\n{backup_directory}\n")
+            print("---------------------- And Then -------------------\n")
+            stone_shard_files = os.listdir(stoneshard_directory)
             for file in stone_shard_files:
-                shutil.copy(stone_shard_directory + "/" + file, backup_directory)
-            print(f"[backup_save]: Copied files from Stoneshard {stone_shard_directory} to Backup {backup_directory}")
-            print("--[backup_save]: DONE --")
+                shutil.copy(stoneshard_directory + "/" + file, backup_directory)
+            print(
+                f"-- [Backup_Save]: Copied Files From:\n\n{stoneshard_directory}\n\n----------------------- To ------------------------\n\n{backup_directory}\n"
+            )
+            print("---------------------------------------------------")
+            print("----------- [Backup_Save]: F I N I S H E D --------")
+            print("---------------------------------------------------")
 
 
 def main():
-    config = load_config("config.json")
+    config = load_config("config.py")
     backup_save(config)
+
 
 if __name__ == "__main__":
     main()
